1b74f208787c6028abc6b99f758439943305ec35
j.song <jaein_song@yonsei.ac.kr>
2017-08-24 13:14:21 +0900

    minor rev

--- cpu_proc.sh
+++ cpu_proc.sh
@@ -1,7 +1,6 @@
 !/bin/bash
 sleep 0.5
 pfidn=`pgrep $task_name`
-echo $task_name $ppn
 pfid=0
 flag=0
 sec=0
@@ -10,6 +9,7 @@ while [ $flag -lt 1 ]; do
 	pfid=`expr $pfid + $pfidn`
 	if [ $pfid -gt 0 ]; then
 		echo 'before'
+		echo $task_name $ppn $pfid
 		echo `taskset -pc $pfid`
 		taskset -pc $ppn $pfid
 		flag=1
@@ -19,6 +19,7 @@ while [ $flag -lt 1 ]; do
 		flag=0
 	fi
 	if [ $sec -gt 9 ]; then
+		echo $task_name $ppn 
 		flag=1
 		echo fail $pfid $flag
 	fi
